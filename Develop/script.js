f2ecf121fe135608cbf5aaebd7198368edaee5b6
Clayton Miller <cbmiller4897@gmail.com>
2021-08-29 20:53:02 -0500

    finished project

--- Develop/script.js
+++ Develop/script.js
@@ -1,191 +1,130 @@
-// Test Scripts
-
-// window.alert('js is working');
-
-// var promptOne = function() {
-//   window.alert("this is prompt number one.");
-// };
-
-var btnWorking = function() {
-  window.alert("this button is working");
-};
-
-var checkValue = function() {
-  // passLowerBooleen;
-  console.log(passInfo.length);
-};
-
-// Button References
-
-var btnGen = document.querySelector('#generate');
-
-var btnPrompt = document.querySelector('#prompt')
-var btnCheck = document.querySelector('#check');
-
-// var passLengthString =  localStorage.getItem("length");
-var passLength = parseInt(localStorage.getItem("length"));
-
-var passLowerUndefined =   localStorage.getItem("lowercase");
-var passLowerTrue =        (JSON.parse(passLowerUndefined))
-var passLowerFalse =       (JSON.parse(passLowerUndefined))
-
-
-var passUpper =   localStorage.getItem("uppercase");
-var passNumeric = localStorage.getItem("numeric");
-var passSpecial = localStorage.getItem("special");
-
-
-
-var passLower = function () {
-  if (localStorage.getItem("lowercase") === "true")  (localStorage.getItem("lowercase") === true) {
-    var lowerValue = (JSON.parse() === true);
-  } else {
-    var lowerValue = (JSON.parse() === false);
-  }
-  console.log(lowerValue)
-};
-
-
-
-
-
-
-
 // Arrays
-
-var arrayEmpty =    []
 var arrayLower =    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var arrayUpper =    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var arrayNumeric =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var arraySpecial =  ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', '{', ']', '}', '|', ';', ':', ',', '<', '.', '>', '/', '?', '`', '~'];
 
+// Variable declarations
+var btnGen = document.querySelector('#generate');
+
+passwordAttribute = {
+  charLength: 8,
+  includeLower: true,
+  includeUpper: true,
+  includeNumeric: true,
+  includeSpecial: true,
+  arrayMaster: [""],
+  passGenerate: "",
+  reset: function(){
+      this.charLength = 8,
+      this.includeLower = true,
+      this.includeUpper = true,
+      this.includeNumeric = true,
+      this.includeSpecial = true,
+      this.arrayMaster = [""],
+      this.passGenerate = ""
+  }
+  };
+
 // Assignment code here
-
-var promptStart = function() {
-
-    // Call promptOne to begin prompt cycle.
-  // promptTwo();
-
-  // Prompt 1: How many characters in password? (between 8-128)
-  // function promptOne() {
-      // Prompt the user with the first question.
-    var length = prompt('How many characters would you like your password to be? Please select a value between 8 and 128.');
-      // If user inputs a value inside range 8-128, then do the following:
-    if (length >= 8 && length <= 128) {
-      // Alert them the length or their password.
-      alert('Your password will be ' + length + ' characters in length.');
-      // Store the value of their length in local storage with key "length".
-      localStorage.setItem("length", length);
-      // Call the function "promptTwo" to continue the prompt cycle.
-      promptTwo();
-      // If they input a value outside range 8-128, then do the following:
-    } else {
-      // alert user to input a value inside ranage.
-      alert('Please input a number between 8 and 128.');
-      // Re-call the function "promptOne" and have the user try again.
-      promptOne();
-    }
-  }
-
-    // Prompt 2: Include lowercase characters?
-  function promptTwo() {
-    var lower = confirm('Would you like your password to include lowercase characters?');
-
-    if (lower === true) {
+// Prompt 1: Character length of password (8-128)?
+function promptLength() {
+  var value = prompt('How many characters would you like your password to be? Please select a value between 8 and 128.');
+  if (value >= 8 && value <= 128) {
+      alert('Your password will be ' + value + ' characters in length.');
+  } else {
+      alert('Invalid number. Please input a number between 8 and 128.');
+      promptLength();
+  } return value;
+}
+// Prompt 2: Include lowercase characters?
+function promptLowercase() {
+  var value = confirm('Would you like your password to include lowercase characters?');
+  if (value === true) {
       alert('Your password WILL contain lowercase characters.');
-      localStorage.setItem("lowercase", true);
-
-    } else {
+      passwordAttribute.arrayMaster = passwordAttribute.arrayMaster.concat(arrayLower);
+  } else {
       alert('Your password will NOT contain lowercase characters.');
-      localStorage.setItem("lowercase", false);
-    }
-    // promptThree();
-  }
-
-    // Prompt 3: Include uppercase characters?
-  function promptThree() {
-    var upper = confirm('Would you like your password to include uppercase characters?');
-    
-    if (upper === true) {
+  } return value;
+}
+// Prompt 3: Include uppercase characters?
+function promptUppercase() {
+  var value = confirm('Would you like your password to include uppercase characters?');
+  if (value === true) {
       alert('Your password WILL contain uppercase characters.');
-      localStorage.setItem("uppercase", true);
-    } else {
+      passwordAttribute.arrayMaster = passwordAttribute.arrayMaster.concat(arrayUpper);
+  } else {
       alert('Your password will NOT contain uppercase characters.');
-      localStorage.setItem("uppercase", false);
-    }
-    promptFour();
-  }
-    // Prompt 4: Include numeric characters?
-  function promptFour() {
-    var numeric = confirm('Would you like your password to include numeric characters?');
-      if (numeric === true) {
-        alert('Your password WILL contain numeric characters.');
-        localStorage.setItem("numeric", true);
-      } else {
-        alert('Your password will NOT contain numeric characters.');
-        localStorage.setItem("numeric", false);
-      }
-      promptFive();
-  }
-    // Prompt 5: Include special characters?
-  function promptFive() {
-    var special = confirm('Would you like your password to include special characters?');
-    
-    if (special === true) {
-      alert('Your password WILL contain special characters.');
-      localStorage.setItem("special", true);
+  } return value;
+}
+// Prompt 4: Include Numeric characters?
+function promptNumeric() {
+  var value = confirm('Would you like your password to include Numeric characters?');
+  if (value === true) {
+      alert('Your password WILL contain Numeric characters.');
+      passwordAttribute.arrayMaster = passwordAttribute.arrayMaster.concat(arrayNumeric);
+
+  } else {
+      alert('Your password will NOT contain Numeric characters.');
+  } return value;
+}
+// Prompt 5: Include Special characters?
+function promptSpecial() {
+  var value = confirm('Would you like your password to include Special characters?');
+  if (value === true) {
+      alert('Your password WILL contain Special characters.');
+      passwordAttribute.arrayMaster = passwordAttribute.arrayMaster.concat(arraySpecial);
+
+  } else {
+      alert('Your password will NOT contain Special characters.');
+  } return value;
+}
+// Prompt 6: If all criteria is false, loop user back through prompts.
+function noGo() {
+  if (
+  passwordAttribute.includeLower === false &&
+  passwordAttribute.includeUpper === false &&
+  passwordAttribute.includeNumeric === false &&
+  passwordAttribute.includeSpecial === false
+  ) {
+      alert("Not enough information to generate a password. Password must include at least one character type. Return to prompt 1 and try again.");
+      writePassword();
     } else {
-      alert('Your password will NOT contain special characters.');
-      localStorage.setItem("special", false);
+      alert('All password criteria inputed correctly. Generating your secure password...');
     }
-    noGo();
-  }
-    // Prompt noGo: This prompt checks if the user inputed enough information to generate a random password.
-  function noGo() {
-
-    // alert('this is the noGo function');
-    // alert(passLength + " " + passLower + " " + passUpper + " " + passNumeric + " and " + passSpecial);
-    
-    if (
-      // passLower === "false" &&
-      // passUpper === "false" &&
-      // passNumeric === "false" &&
-      // passSpecial === "false"
-      localStorage.getItem("lowercase") === "false" &&
-      localStorage.getItem("uppercase") === "false" &&
-      localStorage.getItem("numeric") === "false" &&
-      localStorage.getItem("special") === "false"
-      ) {
-      alert("Not enough information to generate a password. Return to prompt 1 and try again.");
-      promptOne();
-    } else {
-      alert('All password criteria inputed correctly. Click Generate Password button to generate your secure password!');
-    }
-  }
-};
-
-
-  // Write password to the #password input
-
-function writePassword() {
-  alert("this is the writePassword function")
-
 }
 
+// Input new values from prompt and confirm windows 
+var promptStart = function(){
+  passwordAttribute.charLength = promptLength();
+  passwordAttribute.includeLower = promptLowercase();
+  passwordAttribute.includeUpper = promptUppercase();
+  passwordAttribute.includeNumeric = promptNumeric();
+  passwordAttribute.includeSpecial = promptSpecial();
+  noGo();
+};
 
-  // function writePassword() {
+var randomCharPick = function (length){
+  var value = Math.floor(Math.random()* length);
+  return value;
+};
 
-  //   var password = generatePassword();
+var runScript = function(){
+  passwordAttribute.reset();
+  promptStart();
 
+  for (var i = 0; i < passwordAttribute.charLength; i++) {
+    var index = randomCharPick(passwordAttribute.arrayMaster.length);
+    passwordAttribute.passGenerate = passwordAttribute.passGenerate.concat(passwordAttribute.arrayMaster[index]);  
+  } return passwordAttribute.passGenerate
+};
 
-  //   var passwordText = document.querySelector("#password");
-
-  //   passwordText.value = password;
-  // }
+// Write password to the #password input
+function writePassword() {
+  var password = runScript();
+  var passwordDisplay = document.getElementById('password');
+  passwordDisplay.value = password;
+}
 
   // Add event listeners
-
   btnGen.addEventListener("click", writePassword);
-  
-  btnPrompt.addEventListener("click", getPassLength());
-  btnCheck.addEventListener("click", checkValue);
